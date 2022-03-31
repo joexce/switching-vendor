dca258fd2977c9cb4b45bf9646afd2760a4b2e2f
joexce <jonathandrives@gmail.com>
2022-03-31 23:21:26 +0700

    change name

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const middleware = require('./src/middleware');
 const router = require('./src/router/notificationRouter');
-const dotenv = require('dotenv');
 dotenv.config();
 
 const vendorAvailable = process.env.VENDOR_ACTIVATE;
@@ -11,18 +11,15 @@ const getActiveVendor = vendors.split(',');
 
 const app = express();
 const port = process.env.PORT || 8080;
+
 app.locals.vendorActive = getActiveVendor[vendorAvailable];
 
 app.use(bodyParser.json({
     limit: 90000
 }));
 
-app.get("/ping", (req,res) => {
-    res.send("it is works");
-});
-
 router(app, { ...middleware });
 
 app.listen(port, () => {
-    console.log('server is running on port ', port);
+    console.log('server is running on port ', port, ' vendor active:', app.locals.vendorActive);
 })
